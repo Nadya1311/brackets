206459031b4387ccc738189ed2e6414b5ad301ef
Nadya1311 <nadya.samosyuk.88@mail.ru>
2022-02-08 15:14:45 +0300

    feat: add  function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = [];
+  const closedBrackets= [];
+  const arr= [];
+
+ 
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closedBrackets.push(bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (closedBrackets.includes(str[i])){
+
+      const openPairs = openBrackets[closedBrackets.indexOf(str[i])];
+      if (arr[arr.length - 1] === openPairs ){
+        arr.splice(-1,1);
+      } else {
+        arr.push(str[i]);
+      }
+
+    } else {
+      arr.push(str[i]);
+    }
+  }
+  return arr.length === 0;
 }
